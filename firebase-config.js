1c2bae652c0cddb0addc3538b4bf653307a39e5e
Lyskha <Khalyssandescu75@yahoo.com>
2026-06-05 15:26:10 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -14,3 +14,14 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js";
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
